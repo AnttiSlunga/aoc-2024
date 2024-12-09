bc762bc3dc654c445307404c81f0652cc8ac08aa
Antti Slunga <antti.slunga@solita.fi>
2024-12-09 15:14:08 +0200

    Day 7 part 2

--- src/aoc_2024/puzzles/day7.clj
+++ src/aoc_2024/puzzles/day7.clj
@@ -6,9 +6,10 @@
   (if (empty? operator)
     (if (= wanted-result result) wanted-result)
     (let [op (first operator)
-          result (if (= op "+")
-                   (+ result (first equation))
-                   (* result (first equation)))]
+          result (case op
+                   "+" (+ result (first equation))
+                   "*" (* result (first equation))
+                   "||" (Long/parseLong (str result (first equation))))]
       (recur wanted-result (rest equation) (rest operator) result))))
 
 
@@ -31,5 +32,27 @@
                                        (clojure.string/split #" ")))])))]
     (->> input
          (pmap #(valid? %))
+         (remove nil?)
+         (apply +))))
+
+(defn valid2? [[result equation]]
+  (->> (pmap
+         #(match result (rest equation) % (first equation))
+         (combo/selections ["+" "*" "||"] (dec (count equation))))
+       (remove nil?)
+       first))
+
+;;348360680516005
+(defn part2 []
+  (let [input (->> (core/get-input "day7")
+                   (mapv #(let [value (clojure.string/split % #":")]
+                            [(Long/parseLong (first value))
+                             (mapv
+                               (fn [v] (Long/parseLong v))
+                               (-> (second value)
+                                   clojure.string/trim
+                                   (clojure.string/split #" ")))])))]
+    (->> input
+         (pmap #(valid2? %))
          (remove nil?)
          (apply +))))
